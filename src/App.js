7bcd3d1fe009614204b1aa10185b4829aaf936dc
Krishna Farswan <111841120+krishna12-coder@users.noreply.github.com>
2024-08-07 18:35:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import ChatRoom from "./component/ChatRoom";
+import MainForm from "./component/MainForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div
+      className="container-fluid bg-light text-dark d-flex align-items-center justify-content-center"
+      style={{ height: "100vh"}}
+    >
+      <Router>
+        <Routes>
+          <Route index element={<MainForm />}></Route>
+          <Route path="/chat/:roomName" element={<ChatRoom />}></Route>
+          <Route path="*" element={<h1>404 not found!</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
